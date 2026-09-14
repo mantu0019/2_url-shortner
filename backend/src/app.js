9c4858a4a7d3d36e3df131666ec91ad8d47aaedb
mantu kushwaha <mantukumar0019@gmail.com>
2026-09-14 12:00:20 +0530

    fix backend api url

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,11 @@ import cors from "cors"
 const app = express();
 
 app.use(cors({
-    origin:"http://localhost:5173"
+    origin: 
+    [
+    "http://localhost:5173",
+    "https://two-url-shortner-1.onrender.com"
+    ]
 }))
 
 
